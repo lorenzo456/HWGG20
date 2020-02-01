8c195869cc5096299a31e3bc00ec839b597ffe67
ASmithie <44504052+ASmithie@users.noreply.github.com>
2020-02-01 13:17:30 +0000

    WInCOndition

---
+++ Assets/LevelStuff/WinCondition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinCondition : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Finish")
+        {
+            if(this.tag == "Player1")
+            {
+                SceneManager.LoadScene(2);
+            }
+            if(this.tag == "Player2")
+            {
+                SceneManager.LoadScene(3);
+            }
+        }
+    }
+}
