3048fbdb59eb4db8193e2d9280b91ef933023c9e
AlanBarr8 <60344099+AlanBarr8@users.noreply.github.com>
2020-02-01 00:47:30 +0000

    Add files via upload

---
+++ CameraStuff/Scripts/CanvasScalar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasScalar : MonoBehaviour
+{
+
+    public bool bottom;
+    Canvas canvas;
+    RectTransform rectTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
+        rectTransform = GetComponent<RectTransform>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rectTransform.rect.height = canvas.GetComponent<RectTransform>().rect.height / 2;
+        rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.x, canvas.GetComponent<RectTransform>().rect.height / 2);
+        if(bottom)
+            rectTransform.position = new Vector3(rectTransform.position.x, canvas.GetComponent<RectTransform>().rect.height / 2, rectTransform.position.z);
+    }
+}
