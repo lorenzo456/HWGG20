f1fd3cac272f6271139202b0e9fe679e9d2bc1ba
ITheCon <cksmith@net-ex.no>
2020-02-01 01:23:57 +0000

    Can only jump when on ground
    now you can only jump when colliding with the ground, with a small grace period when leaving to vary jump height.

--- Assets/CarFolder/Scripts/Car_Movement.cs
+++ Assets/CarFolder/Scripts/Car_Movement.cs
@@ -5,21 +5,34 @@ using UnityEngine;
 public class Car_Movement : MonoBehaviour
 {
     public float speed = 3.0f;
-    public float jump = 20.0f;
+    public float jump = 80.0f;
     public float MaxSpeed = 15.0f;
+    private int jumpHeight = 0;
     private Rigidbody2D rigidBody;
     private Vector2 velocity;
+    private Collider2D carCollider;
+    bool touchingGround = false;
     bool personInCar = true;
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = gameObject.GetComponent<Rigidbody2D>();
+        carCollider = gameObject.GetComponent<Collider2D>();
         velocity = rigidBody.velocity;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (carCollider.IsTouching(GameObject.Find("groundofGOD").GetComponent<Collider2D>()))
+        {
+            touchingGround = true;
+        }
+        else
+        {
+            touchingGround = false;
+        }
+
         if (personInCar)
         {
             keyMovement();
@@ -46,8 +59,18 @@ public class Car_Movement : MonoBehaviour
 
     void keyJump()
     {
+        if (touchingGround)
+        {
+            jumpHeight = 0;
+
+        }
+        else if(jumpHeight <= 21)
+        {
+            jumpHeight++;
+        }
+
         float verticalVelocity = velocity.y;
-        if (Input.GetKey(KeyCode.UpArrow) && verticalVelocity <= 3*jump)
+        if (Input.GetKey(KeyCode.UpArrow) && verticalVelocity <= 7*jump && jumpHeight <= 20 )
         {
             Vector2 horizontalForce = new Vector2(0.0f, jump);
             rigidBody.AddForce(horizontalForce);
