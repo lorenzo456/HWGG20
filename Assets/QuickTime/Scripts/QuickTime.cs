a71aa726602c3fdc7090a7fb426821b44a371db9
ASmithie <44504052+ASmithie@users.noreply.github.com>
2020-02-01 22:01:39 +0000

    QT counts
    Need to find a way to remove the image with out causing errors

--- Assets/QuickTime/Scripts/QuickTime.cs
+++ Assets/QuickTime/Scripts/QuickTime.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class QuickTime : MonoBehaviour
 {
-
+    public bool QTPlay;
     public int arrowSpeed;
     public float distanceFromCircle;
     public GameObject[] arrows;
@@ -18,48 +18,72 @@ public class QuickTime : MonoBehaviour
 
     public float deletedist;
 
+    public enum PlayerTag { Player, Player2 };
+    public PlayerTag player;
+    private KeyCode up, down, left, right;
+    public int points = 0;
+
+    public float perfectDist;
+    public float goodDist;
+    public float badDist;
+
     void Start()
     {
+       
+        up = KeyCode.W;
+        down = KeyCode.S;
+        left = KeyCode.A;
+        right = KeyCode.D;
 
-        newParent = Instantiate(parent, this.transform);
+        //up = KeyCode.UpArrow;
+        //down = KeyCode.DownArrow;
+        //left = KeyCode.LeftArrow;
+        //right = KeyCode.RightArrow;
+
         
+        newParent = Instantiate(parent, this.transform);
         SpawnArrow();
+        
     }
 
     void SpawnArrow()
     {
-        int arrowType = Random.Range(0, 3);
-        GameObject newarrow;
-        Vector3 moveDirection;
+        if (QTPlay)
+        {
+            int arrowType = Random.Range(0, 3);
+            GameObject newarrow;
+            Vector3 moveDirection;
 
-        if (arrowType == 0)
-        {
-            moveDirection = Vector3.up;
-            newarrow = Instantiate(arrows[0], new Vector3(0f, 0f, 0f), Quaternion.Euler(0f,0f,0f));
-            newarrow.transform.position += moveDirection * -distanceFromCircle;
+            if (arrowType == 0)
+            {
+                moveDirection = Vector3.up;
+                newarrow = Instantiate(arrows[0], new Vector3(0f, 0f, 0f), Quaternion.Euler(0f,0f,0f));
+                newarrow.transform.position += moveDirection * -distanceFromCircle;
+            }
+            else if(arrowType == 1)
+            {
+                moveDirection = Vector3.left;
+                newarrow = Instantiate(arrows[1], new Vector3(0f, 0f, 0f), Quaternion.Euler(0f, 0f, 90f));
+                newarrow.transform.position += moveDirection * -distanceFromCircle;
+            }
+            else if (arrowType == 2)
+            {
+                moveDirection = Vector3.down;
+                newarrow = Instantiate(arrows[2], new Vector3(0f, 0f, 0f), Quaternion.Euler(0f, 0f, 180f));
+                newarrow.transform.position += moveDirection * -distanceFromCircle;
+            }
+            else
+            {
+                moveDirection = Vector3.right;
+                newarrow = Instantiate(arrows[3], new Vector3(0f, 0f, 0f), Quaternion.Euler(0f, 0f, -90f));
+                newarrow.transform.position += moveDirection * -distanceFromCircle;
+            }
+            newarrow.AddComponent<Rigidbody2D>();
+            newarrow.transform.parent = newParent.transform;
+            Debug.Log("" + newarrow.transform.parent.name);
+            MoveArrow(newarrow, moveDirection);
         }
-        else if(arrowType == 1)
-        {
-            moveDirection = Vector3.left;
-            newarrow = Instantiate(arrows[1], new Vector3(0f, 0f, 0f), Quaternion.Euler(0f, 0f, 90f));
-            newarrow.transform.position += moveDirection * -distanceFromCircle;
-        }
-        else if (arrowType == 2)
-        {
-            moveDirection = Vector3.down;
-            newarrow = Instantiate(arrows[2], new Vector3(0f, 0f, 0f), Quaternion.Euler(0f, 0f, 180f));
-            newarrow.transform.position += moveDirection * -distanceFromCircle;
-        }
-        else
-        {
-            moveDirection = Vector3.right;
-            newarrow = Instantiate(arrows[3], new Vector3(0f, 0f, 0f), Quaternion.Euler(0f, 0f, -90f));
-            newarrow.transform.position += moveDirection * -distanceFromCircle;
-        }
-        newarrow.AddComponent<Rigidbody2D>();
-        newarrow.transform.parent = newParent.transform;
-        Debug.Log("" + newarrow.transform.parent.name);
-        MoveArrow(newarrow, moveDirection);
+        
     }
 
     void MoveArrow(GameObject newarrow, Vector3 moveDirection)
@@ -68,6 +92,36 @@ public class QuickTime : MonoBehaviour
         newarrow.GetComponent<Rigidbody2D>().AddForce(moveDirection * arrowSpeed);
         arrowMove = true;
     }
+
+
+    void CheckKey(string key)
+    {
+        if (key == newParent.transform.GetChild(0).tag)
+        {
+            float currentDist = Vector3.Distance(newParent.transform.GetChild(0).transform.position, newParent.transform.position);
+            if(currentDist < perfectDist)
+            {
+                Debug.Log("50");
+                points += 50;
+            }
+            else if(currentDist < goodDist)
+            {
+                Debug.Log("25");
+                points += 25;
+            }
+            else if(currentDist < badDist)
+            {
+                Debug.Log("10");
+                points += 10;
+            }
+            else
+            {
+                Debug.Log("1");
+                points += 1;
+            }
+        }
+    }
+
 
     void Update()
     {
@@ -80,5 +134,32 @@ public class QuickTime : MonoBehaviour
                 SpawnArrow();
             }
         }
+
+
+        if(Input.GetKeyDown(up))
+        {
+            CheckKey("up");
+        }
+        if(Input.GetKeyDown(down))
+        {
+            CheckKey("down");
+        }
+        if (Input.GetKeyDown(left))
+        {
+            CheckKey("left");
+        }
+        if (Input.GetKeyDown(right))
+        {
+            CheckKey("right");
+        }
+
+        //todo end of minigame
+        if(points >= 100)
+        {
+            QTPlay = false;
+            points = 0;
+        }
     }
+
+    
 }
