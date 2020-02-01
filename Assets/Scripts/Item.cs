99dc3e9937778218c188dacb3c03b1e74215853c
SolomonBaarda <solomon_baarda@outlook.com>
2020-02-01 10:06:37 +0000

    Update Item.cs

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Item : MonoBehaviour
 {
+    public static UnityEvent OnItemCollision<GameObject>(); 
 
     // Start is called before the first frame update
     void Start()
