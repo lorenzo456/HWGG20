e26cc257c159a7e274bef5f016a8f9090543d181
lorenzo456 <lorenzo456@users.noreply.github.com>
2020-02-02 14:57:31 +0000

    added menus

--- Assets/LevelStuff/WinCondition.cs
+++ Assets/LevelStuff/WinCondition.cs
@@ -8,11 +8,11 @@ public class WinCondition : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         
-        if(other.gameObject.tag == "Player")
+        if(other.gameObject.tag == "Car")
         {
             SceneManager.LoadScene(2);
         }
-        if(other.gameObject.tag == "Player2")
+        if(other.gameObject.tag == "Car2")
         {
             SceneManager.LoadScene(3);
         }
