71bcb5761e94fc9ab1eb885b937a3c9c7ede54a0
lorenzo456 <lorenzo456@users.noreply.github.com>
2020-02-02 13:27:32 +0000

    Updated camera and level prefab

--- Assets/CameraStuff/Scripts/NoRotationCamera.cs
+++ Assets/CameraStuff/Scripts/NoRotationCamera.cs
@@ -7,8 +7,14 @@ public class NoRotationCamera : MonoBehaviour
     public Transform car;
 
     // Update is called once per frame
+
+    void Start()
+    {
+        transform.parent = null;
+    }
     void Update()
     {
+        transform.position = new Vector3(car.position.x,car.position.y + 3,transform.position.z);
         transform.rotation = Quaternion.Euler(0, 0, car.rotation.z);
     }
 }
